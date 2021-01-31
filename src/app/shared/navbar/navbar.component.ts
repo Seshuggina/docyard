f18ab52ac0b326a4fb34da899a34b72b254a9350
Seshu Vuggina <seshuggina@gmail.com>
2021-01-31 07:45:03 +0530

    Navbar issue fixed

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
@@ -7,9 +7,14 @@ import { Globals } from '../services/globals.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  host: {
+    '(window:resize)': 'onResize($event)'
+  }
 })
+
 export class NavbarComponent implements OnInit, OnDestroy {
+
 
   searchText:string;
   showMainSearch:boolean;
@@ -31,9 +36,24 @@ export class NavbarComponent implements OnInit, OnDestroy {
     this.showMainSearch = !this.showMainSearch;
   }
 
+  onResize(event) {
+    console.log("event.target.innerWidth;", event.target.innerWidth);
+    if(window.location.pathname !== '/') {
+      if(event.target.innerWidth > 991.98) {
+        this.showSearchButton = false;
+        this.showMainSearch = true;
+      } else if(!this.showMainSearch || !this.showSearchButton) {
+        this.showMainSearch = true;
+        this.showSearchButton = true;
+      }
+    }
+  }
+
   ngOnDestroy() {
+    
     this.unsubscribe.next();
     this.unsubscribe.complete();
   }
+  
 
 }
